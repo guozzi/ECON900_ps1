162c6e37c8f4fd3a4c4c34942eb667a70390b6f9
Guo Li <liguo.us@hotmail.com>
2019-04-23 15:21:04 -0400

    parsed all rankings, names, ratings and votes

--- boardgamegeek.com_parse.py
+++ boardgamegeek.com_parse.py
@@ -17,12 +17,21 @@ for one_file_name in glob.glob("html_files/*.html"):
 	game_rows = soup.find_all("tr", {"id": "row_"})
 	
 	for r in game_rows:
-		Game_Rank = r.find("td", {"class": "collection_rank"}).text
-		
+		Rank = r.find("td", {"class": "collection_rank"}).text
+		Name = r.find("td", {"class": "collection_objectname"}).text
+		Geek_Rating = r.find("td", {"class": "collection_bggrating"}).text
+		Avg_Rating = r.find("td", {"class": "collection_bggrating"}).find_next_sibling("td").text
+		Votes = r.find("td", {"class": "collection_bggrating"}).find_next_sibling("td").find_next_sibling("td").text
+
+
+
 		df = df.append({
-			'Parsed_From': Parsed_From, #Indicates the corresponding html file 
-			'Game_Rank': Game_Rank, # Overall rank of the game, N/A means not ranked
-			
+			'Parsed_From': Parsed_From, # Indicates the corresponding html file 
+			'Rank': Rank, # Overall rank of the game, N/A means not ranked
+			'Name': Name, # Name of the game
+			'Geek_Rating': Geek_Rating, # Rating given by boardgamegeek.com
+			'Avg_Rating': Avg_Rating, # Average rating given by users
+			'Votes': Votes # Total user votes
 			}, ignore_index=True)
 
 
